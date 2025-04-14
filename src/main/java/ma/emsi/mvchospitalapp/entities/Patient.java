f6340ce76a1be0d9f94e5d6e060b38bdb3fb6f23
oussama Essalmani <essalmanioussama@gmail.com>
2025-04-14 01:24:25 +0100

    v2

--- src/main/java/ma/emsi/mvchospitalapp/entities/Patient.java
+++ src/main/java/ma/emsi/mvchospitalapp/entities/Patient.java
@@ -2,7 +2,10 @@ package ma.emsi.mvchospitalapp.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,15 +13,22 @@ import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
-@Data @NoArgsConstructor
-@AllArgsConstructor @Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Patient {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nom;
     private String gender;
+
+    @Temporal(TemporalType.DATE)
     private Date dataNissance;
+
     private boolean malade;
     private int score;
 }
